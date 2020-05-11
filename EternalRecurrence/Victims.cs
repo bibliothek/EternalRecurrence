a07264f9c5ab2fc4214eeb027580a3559ec190f2
Martin Thaller <thaller.martin@gmail.com>
2020-05-11 08:51:04 +0200

    Update Victims.cs

--- EternalRecurrence/Victims.cs
+++ EternalRecurrence/Victims.cs
@@ -8,7 +8,6 @@ internal static class Victims
 
     private static Dictionary<string, string> victims = new Dictionary<string, string> {
             {"steml", "U59F531H7"},
-            {"maber", "U1D7UQT6V"},
             {"ivapo", "UR3C5Q1ST"},
             {"flole", "UD3H5EV40"},
             {"matha", "U0C3HCB7C"},
